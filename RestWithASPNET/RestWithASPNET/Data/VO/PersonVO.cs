185d27159ceda9d642ee6b2f850f53c8da014e67
Julio Veiga <jcbvbr@gmail.com>
2021-02-20 17:41:40 -0300

    Add custom serialization

--- RestWithASPNET/RestWithASPNET/Data/VO/PersonVO.cs
+++ RestWithASPNET/RestWithASPNET/Data/VO/PersonVO.cs
@@ -1,12 +1,19 @@
-﻿namespace RestWithASPNET.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNET.Data.VO
 {
 
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
-        public string FirstName { get; set; }    
-        public string LastName { get; set; }    
-        public string Address { get; set; }    
+        [JsonPropertyName("name")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
 
     }
